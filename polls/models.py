e79f5bdb5754cff0b1e82609c77e5f22e91b6fc5
adityanagdekar <adityanagdekar@gmail.com>
2025-09-30 12:57:29 -0400

    Clean repo for EB deploy

---
+++ polls/models.py
@@ -0,0 +1,12 @@
+import datetime
+from django.db import models
+from django.utils import timezone
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
---
+++ polls/views.py
@@ -0,0 +1,29 @@
+from django.db.models import F
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
+from django.views import generic
+
+from .models import Task
+
+# List all tasks
+def task_list(request):
+    tasks = Task.objects.filter(is_completed=False).order_by("-created_at")
+    return render(request, "polls/list.html", {"tasks": tasks})
+
+# Add a new task
+def add_task(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        description = request.POST.get("description")
+        if title:  # title is required
+            Task.objects.create(title=title, description=description)
+        return redirect("polls:task_list")
+    return render(request, "polls/add.html")
+
+# Mark a task as complete
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.is_completed = True
+    task.save()
+    return redirect("polls:task_list")
