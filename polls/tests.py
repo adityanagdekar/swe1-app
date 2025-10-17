f180a69e141f68ebb2d1d5d91bbe9a6aa3dc5dd4
adityanagdekar <adityanagdekar@gmail.com>
2025-10-17 19:52:37 -0400

    Resolving flake8 issues, adding new test-cases

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Task
 
-# Create your tests here.
+
+class TaskModelTest(TestCase):
+    def test_task_creation(self):
+        task = Task.objects.create(title="Test Task", description="Test Description")
+        self.assertEqual(task.title, "Test Task")
+        self.assertFalse(task.is_completed)
+
+    def test_task_str(self):
+        task = Task.objects.create(title="Test Task")
+        self.assertEqual(str(task), "Test Task")
+
+
+class TaskViewTest(TestCase):
+    def test_task_list_view(self):
+        response = self.client.get("/polls/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Todo List")
+
+    def test_add_task(self):
+        response = self.client.post(
+            "/polls/add/", {"title": "New Task", "description": "New Description"}
+        )
+        self.assertEqual(Task.objects.count(), 1)
+        self.assertEqual(response.status_code, 302)  # Redirect
